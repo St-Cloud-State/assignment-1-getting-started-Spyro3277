63dcc80f63add85803f155cae1f33d5bcabfecf7
Spyro3277 <144630755+Spyro3277@users.noreply.github.com>
2025-01-29 18:59:34 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,55 @@
 from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
-books = ["book1"];
+allApps = []
 
 
+#Application class
+class application:
+    def __init__(self,name, appNum, status):
+        self.name = name
+        self.appNum = appNum
+        self.status = status
 
-@app.route('/api/books', methods=['GET'])
-def get_all_books_v2():
-    return jsonify({'list of books':books})
-
-
-# API to add a book to the database
-@app.route('/api/add_book', methods=['POST'])
-def add_book():
-    print("adding book")
+#adds in a new application
+@app.route('/api/add_app', methods=['POST'])
+def add_app():
+    
+    print("Adding an application")
     data = request.get_json()
-    title = data.get('title')
-    books.append(title);
-    return jsonify({'message': 'Book added successfully'})
- 
-# Route to render the index.html page
+    name = data.get('name')
+    status = "recieved"
+
+    
+    newApplication = application(name,len(allApps)+1,status)
+    
+    allApps.append(newApplication)
+   
+    
+    return jsonify({'message': 'Application added successfully'})
+
+#Checks the status of an application
+@app.route('/api/check_status', methods=['POST'])
+def check_status():
+    print("Checking application status")
+
+    return jsonify({'message': 'Checking application status'})
+
+
+#Api to update the status of the application
+@app.route('/api/update_status', methods=['POST']) 
+def update_status():
+    print("Updating status")
+
+    data = {"newStatus": {'notFound', 'received', 'processing', 'accepted', 'rejected'}}
+    data["newStatus"] = list(data["newStatus"])
+
+    return jsonify(data)
+
+#returns the render template
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=8080)
